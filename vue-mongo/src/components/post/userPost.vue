08b47d16f64424245023446e011b87b9b4428211
ambassadherdev2 <ambassadherdev2@anuitex.com>
2019-05-29 10:37:37 +0300

    userPost fix

--- vue-mongo/src/components/post/userPost.vue
+++ vue-mongo/src/components/post/userPost.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-layout row wrap v-if="userPosts.length === 0">Please create your post</v-layout>
+    <v-layout row wrap v-if="userPosts.length === 0">Please create your EditPost</v-layout>
     <v-layout row wrap v-else>
       <v-flex xs12 sm6 md4 lg3 padding-custom v-for="(post, i) in userPosts" :key="i">
         <v-card class="card-custom">
